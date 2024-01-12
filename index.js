9ae38bec24d57778c8876c2a780c7c7f414f9fa2
Avni <2025agarg@tjhsst.edu>
2024-01-11 22:56:19 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use("/css", express.static("./node_modules/bootstrap/dist/css"));
+
+// -------------- routes -------------- //
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+// -------------- listener -------------- //
+const listener = app.listen(
+    process.env.PORT || 8080,
+    process.env.HOST || "127.0.0.1",
+    function () {
+        console.log("Express server started on port 8080");
+        console.log("Visit locolhost:8080 in your browser or 127.0.0.1:8080!")
+    }
+);
