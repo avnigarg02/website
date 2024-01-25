5d2ed88e5215bbf60e3e5b0e5a0197239d1ec6ab
Avni <2025agarg@tjhsst.edu>
2024-01-25 11:06:04 -0500

    cookies

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express')
 const app = express()
 
+var cookieParser = require('cookie-parser')
+app.use(cookieParser());
+
 app.set('view engine', 'ejs')
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
+
+function get_theme(req, res) {
+    let { dark_mode } = req.cookies
+
+    if (dark_mode === undefined) {
+        dark_mode = true
+        res.cookie('dark_mode', dark_mode, {});
+    }
+
+    return dark_mode
+}
 
 // -------------- routes -------------- //
 const games = require('./routes/games.js')
 app.use(games);
 
 app.get('/', (req, res) => {
-    res.render('index')
+    let theme = get_theme(req, res) ? 'night' : 'winter'
+    res.render('index', { theme: theme })
 })
 
 app.get('/contact', (req, res) => {
